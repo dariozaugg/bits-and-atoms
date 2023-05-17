2bf757caf265492fb08abcc6304342076a51196b
dariozaugg <30509108+dariozaugg@users.noreply.github.com>
2023-05-17 12:00:54 +0200

    We started with JS

---
+++ script.js
@@ -0,0 +1,19 @@
+// alert("You don't suck");
+// console.log("I Like you");
+
+// let user = "Dario",
+//   age = 30,
+//   message = "even cooler guy";
+// console.log(user + " is " + age + " and a " + message);
+
+//let icon = "";
+
+// const currentWeather = "rainy";
+
+// if (currentWeather == "rainy") {
+//   console.log("🌧️");
+// } else {
+//   console.log("☀️");
+// }
+
+const myObject = {};
