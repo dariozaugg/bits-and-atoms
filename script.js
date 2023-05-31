9026b5080addb7c0edca6cb022985e9462d0c47b
dariozaugg <30509108+dariozaugg@users.noreply.github.com>
2023-05-31 23:47:32 +0200

    Card 2
    Did the main functions of it

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-// alert("You don't suck");
-// console.log("I Like you");
+const pageUrls = [
+  "http://127.0.0.1:5500/js-card1.html",
+  "http://127.0.0.1:5500/js-card2.html",
+  "http://127.0.0.1:5500/js-card3.html",
+  "http://127.0.0.1:5500/js-card4.html",
+];
 
-// let user = "Dario",
-//   age = 30,
-//   message = "even cooler guy";
-// console.log(user + " is " + age + " and a " + message);
+function isScrollAtBottom() {
+  const scrollTrigger = document.getElementById("scrollTrigger");
+  const triggerPosition = scrollTrigger.getBoundingClientRect().top;
 
-//let icon = "";
+  return triggerPosition <= window.innerHeight;
+}
 
-// const currentWeather = "rainy";
+// Function to get the current page index from the URL
+function getCurrentPageIndex() {
+  // Get the current URL
+  var currentUrl = window.location.href;
 
-// if (currentWeather == "rainy") {
-//   console.log("🌧️");
-// } else {
-//   console.log("☀️");
-// }
+  // Find the index of the current URL in the pageUrls array
+  var currentPageIndex = pageUrls.findIndex((url) => url === currentUrl);
 
-const myObject = {};
+  if (currentPageIndex > -1) {
+    return currentPageIndex;
+  } else {
+    console.log("Unable to determine the current page index from the URL.");
+    return 0;
+  }
+}
+
+// Function to load the next page
+function loadNextPage() {
+  // Get the current page index based on the current URL
+  var currentPageIndex = getCurrentPageIndex();
+
+  // Determine the next page index
+  var nextPageIndex = currentPageIndex + 1;
+
+  // If the next page index is within the valid range, navigate to the corresponding URL
+  if (nextPageIndex < pageUrls.length) {
+    var nextPageUrl = pageUrls[nextPageIndex];
+    window.location.href = nextPageUrl;
+  } else {
+    window.location.href = pageUrls[0];
+  }
+}
+
+// Event listener for scroll
+window.addEventListener("scroll", function () {
+  if (isScrollAtBottom()) {
+    loadNextPage();
+  }
+});
